9ecf54f805e02e3b714636b816f6cec51daf910e
晴风 <773939719@qq.com>
2024-11-28 02:39:32 +0800

    update

---
+++ 4.package.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+source "$(pwd)/config.sh"
+
+echo "Updating package file..."
+PACKAGE_STRING=""
+sed -i '' -e "s/let release =.*/let release = \"$FFMPEG_KIT_TAG\"/" Package.swift
+
+XCFRAMEWORK_DIR="$WORK_DIR/prebuilt/bundle-apple-xcframework"
+
+rm -rf $XCFRAMEWORK_DIR/*.zip
+
+for f in $(ls "$XCFRAMEWORK_DIR")
+do
+    echo "Adding $f to package list..."
+    PACAKGE="$XCFRAMEWORK_DIR/$f"
+    ditto -c -k --sequesterRsrc --keepParent $PACAKGE "$PACAKGE.zip"
+    PACKAGE_NAME=$(basename "$f" .xcframework)
+    PACKAGE_SUM=$(sha256sum "$PACAKGE.zip" | awk '{ print $1 }')
+    PACKAGE_STRING="$PACKAGE_STRING\"$PACKAGE_NAME\": \"$PACKAGE_SUM\", "
+done
+
+PACKAGE_STRING=$(basename "$PACKAGE_STRING" ", ")
+sed -i '' -e "s/let frameworks =.*/let frameworks = [$PACKAGE_STRING]/" Package.swift
+
+echo "Copying License..."
+cp -f src/ffmpeg-kit/LICENSE ./
+
+echo "Creating Release..."
+gh release create -p -d $FFMPEG_KIT_TAG -t "QFFmpegKit $FFMPEG_KIT_TAG" --generate-notes --verify-tag
+
+echo "Uploading Binaries..."
+for f in $(ls "$XCFRAMEWORK_DIR")
+do
+    if [[ $f == *.zip ]]; then
+        gh release upload $FFMPEG_KIT_TAG "$XCFRAMEWORK_DIR/$f"
+    fi
+done
+
+gh release edit $FFMPEG_KIT_TAG --draft=false
+
+echo "3 done"
